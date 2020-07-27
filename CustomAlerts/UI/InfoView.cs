3f87818f0cabc313e38e08eac58d379377a3eeb4
leogandmars <legoandmars@gmail.com>
2020-07-26 18:24:44 -0700

    added names to the top modelers, and a proper link to the CustomAlerts discord server

--- CustomAlerts/UI/InfoView.cs
+++ CustomAlerts/UI/InfoView.cs
@@ -1,11 +1,16 @@
 ﻿using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.ViewControllers;
+using UnityEngine;
 
 namespace CustomAlerts.UI
 {
     [HotReload]
     public class InfoView : BSMLAutomaticViewController
     {
-
+        [UIAction("join-discord")]
+        protected void AlertClicked()
+        {
+            Application.OpenURL("https://discord.gg/MwqzVb");
+        }
     }
 }
