d248e47f76c3775551741300da5e145520035e51
leogandmars <legoandmars@gmail.com>
2020-07-30 18:12:59 -0700

    Fixed priority being wrong type

--- CustomAlerts/Streamlabs/Message.cs
+++ CustomAlerts/Streamlabs/Message.cs
@@ -8,7 +8,7 @@
         public string Name { get; set; }
         public string IsTest { get; set; }
         public string Amount { get; set; }
-        public string Priority { get; set; }
+        public int Priority { get; set; }
         public string ChannelPointsName { get; set; }
     }
 }
