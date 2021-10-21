1780d90421c347d1c0f92c6bcc6b7a755be87452
Eris <erisapps@outlook.be>
2021-10-22 01:26:39 +0200

    Fixed alert icon in list

--- CustomAlerts/UI/Models/ModelCell.cs
+++ CustomAlerts/UI/Models/ModelCell.cs
@@ -7,14 +7,10 @@ namespace CustomAlerts.UI.Models
     {
         public CustomAlert Alert { get; }
 
-        public ModelCell(CustomAlert alert) : base(alert.Descriptor.alertName, $"{alert.Descriptor.authorName} [{alert.AlertType}]", null)
+        public ModelCell(CustomAlert alert)
+            : base(alert.Descriptor.alertName, $"{alert.Descriptor.authorName} [{alert.AlertType}]", BeatSaberMarkupLanguage.Utilities.LoadSpriteFromTexture(alert.Descriptor.icon))
         {
             Alert = alert;
-
-            // TODO: Fix icon got changed into Sprite instead of Texture2D
-            /*text = Alert.Descriptor.alertName;
-            subtext = $"{Alert.Descriptor.authorName} [{Alert.AlertType}]";
-            icon = alert.Descriptor.icon;*/
         }
     }
 }
